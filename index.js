bb1aa5c59ab71fc1a07fbf9e508cb73312ca87ae
lisiurday <lisiurday@gmail.com>
2018-08-01 08:55:55 +0800

    feat: add service.d.ts

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const yaml = require('./lib/yaml')
 const codegen = require('./lib/codegen')
 
 module.exports = conf => {
-    config.init(conf)
-    yaml.convert().then(data => {
-        codegen.generate(data)
-    }).catch(e => {
-        console.error(e.message)
+  config.init(conf)
+  yaml
+    .convert()
+    .then(data => {
+      codegen.generate(data)
+    })
+    .catch(e => {
+      console.error(e.message)
     })
 }
